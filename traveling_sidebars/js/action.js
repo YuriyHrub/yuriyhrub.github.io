b6a6df5ecec3b1656b4322582daf174fea198de4
yuriyHrub <47758858+YuriyHrub@users.noreply.github.com>
2020-02-08 20:27:56 +0200

    Update files

--- traveling_sidebars/js/action.js
+++ traveling_sidebars/js/action.js
@@ -1,6 +1,6 @@
 'use strict';
 window.addEventListener('load', function() {
-  let centralBlock = document.querySelector('.central-block'),
+  let mainBlock    = document.querySelector('.central-block'),
       wrapper      = document.querySelector('.wrapper'),
       blockWidth   = 1200,
       sidebarWidth = 250,
@@ -8,30 +8,35 @@ window.addEventListener('load', function() {
       rightBtn     = document.querySelector('.right-btn');
 
   document.querySelectorAll('.sidebar').forEach(sidebar => sidebar.style.width = `${sidebarWidth}px`);
-  document.addEventListener('click', function(event) {
-    
+  // 
+  wrapper.style.paddingLeft = leftBtn.offsetWidth + 'px';
+  wrapper.style.paddingRight = rightBtn.offsetWidth + 'px';
+
+  document.addEventListener('click', function(event) { 
     if(event.target.classList.contains('btn')) {
+      
       let windowWidth  = document.documentElement.clientWidth,
-          sidebar      = event.target.closest('.sidebar');
+          sidebar      = event.target.closest('.sidebar'),
+          offset       = parseInt(getComputedStyle(mainBlock).marginLeft);
 
-      if(windowWidth - sidebarWidth < blockWidth) {
-
+      if(offset >= 0 && offset <= sidebarWidth) {
+  
         if(!sidebar.classList.contains('hidden')) {
-          if(sidebar.classList.contains('left-sidebar')) {
-            wrapper.style.marginLeft = '0px';
-          } else { wrapper.style.marginRight = '0px'; }
+          // when sidebar hidding
+          sidebar.classList.contains('left-sidebar') ? 
+            wrapper.style.marginLeft = 0 :
+            //right bnt click
+            wrapper.style.marginRight = 0;
+        } else {
+          sidebar.classList.contains('left-sidebar') ?
+            wrapper.style.marginLeft = sidebarWidth + 'px' :
+          // right btn click
+            wrapper.style.marginRight = sidebarWidth + 'px';
         }
-        else {
-          if(sidebar.classList.contains('left-sidebar')) {
-            wrapper.style.marginLeft = sidebarWidth + 'px';
-          } else { wrapper.style.marginRight = sidebarWidth + 'px';}
-        }
-      }
-      else if(centralBlock.offsetWidth + sidebarWidth === windowWidth) {
-        wrapper.style.width = centralBlock.offsetWidth - sidebarWidth + 'px'
       }
 
-      sidebar.classList.toggle('hidden');
+    sidebar.classList.toggle('hidden');
+
     }
   })
 
